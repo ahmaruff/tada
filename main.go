816c6e6efe60788a644e0220535259cf5f5ec181
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-09-14 08:30:55 +0700

    chore: move main.go into root dir

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ahmaruff/tada/cmd/cli"
+	"github.com/ahmaruff/tada/cmd"
 )
 
 func main() {
-	if err := cli.Execute(); err != nil {
+	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
